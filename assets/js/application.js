a89175433a9907dc090869df7ed5d3807b574a5b
anikrauth <anikrouthwebdev@fmail.com>
2021-02-14 00:52:47 +0600

    Don't download this files

---
+++ assets/js/application.js
@@ -0,0 +1,22 @@
+
+	// colletcing recent leaking news for you
+	var typed6 = new Typed('#leak', {
+		strings: ['<span>Act</span>ivism colletcing recent leaking news...^1000\n`<br><a class="see" href="#news-area">see news!</a>'],
+		typeSpeed: 200,
+		backSpeed: 50,
+		loop: false
+	});
+
+
+	// slider
+
+	// $(".owl-carousel").owlCarousel({
+	// 	loop: false,
+	// 	autoplay: true,
+	// 	items: 1,
+	// 	vertical:true,
+	// 	autoplaySpeed: 200,
+	// 	autoplayHoverPause: true,
+	// 	// animateOut: 'slideOutUp',
+	// 	// animateIn: 'slideInUp'
+	// });
